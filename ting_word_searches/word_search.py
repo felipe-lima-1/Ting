a8b215efd3d2134e4a07a799ec230e097aed662a
felipe-lima-1 <luizbrutante@gmail.com>
2023-06-13 14:49:04 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    word = word.lower()
+    result = []
+
+    for file_name in instance.queue:
+        txt = txt_importer(file_name)
+        occurrences = [
+            {"linha": line_num + 1}
+            for line_num, line in enumerate(txt)
+            if word in line.lower()]
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": occurrences
+                })
+    return result
 
 
 def search_by_word(word, instance):
